3a76d90566e1b838978a282c2b7cb1c685d69d97
RandomG <sun4151@gmail.com>
2024-02-26 12:06:46 +0200

    latest tesk

--- app/main.py
+++ app/main.py
@@ -1,23 +1,38 @@
 from fastapi import FastAPI
-import uvicorn
 from fastapi.responses import PlainTextResponse
 from requests_html import AsyncHTMLSession,HTML
 from bs4 import BeautifulSoup
 from rfeed import *
-
-
+# from playwright.async_api import async_playwright
 
 app = FastAPI()
 
 
+# async def get_rendered_html(url):
+#     async with async_playwright() as p:
+#         browser = await p.chromium.launch()
+#         page = await browser.new_page()
+#         await page.goto(url)
+#         # Wait for the page to render completely
+#         await page.wait_for_load_state("networkidle")
+#         # Retrieve the HTML content
+#         html_content = await page.content()
+#         await browser.close()
+#         return html_content
+
 
 
 @app.get("/")
-async def root():
+async def main():
     session = AsyncHTMLSession()
     r = await session.get("https://wizdom.xyz/")
     await r.html.arender()
-    soup = BeautifulSoup(r.html.html,"html.parser")
+    text = r.html.html
+    # text = await get_rendered_html("https://wizdom.xyz/")
+    print(text)
+    print("render achieved")
+    print("now soup....")
+    soup = BeautifulSoup(text,"html.parser")
     movies = []
     for movie_card in soup.findAll("div",attrs={"class": "poster col-md-3 col-lg-2 col-xl-1 col-6"}):
         imdb = movie_card.find('a').attrs["href"].split("/movie/")[1]
@@ -35,3 +50,4 @@ async def root():
                     )
 
     return PlainTextResponse(feed.rss())
+
